9f112dae3ef653094a13adb84d79e4045e4330ea
Maxime <machanwei@gmail.com>
2025-04-09 12:00:19 +0800

    feat: 增加執行容器時的 port 參數支援

--- scripts/utils/enviroment.sh
+++ scripts/utils/enviroment.sh
@@ -257,6 +257,25 @@ exec_bash_in_deploy_env_with_projects() {
     bash
 }
 
+exec_script_in_container_with_project_and_port() {
+    PROJECT_NAME=$1
+    DOCKER_IMAGE=$2
+    SCRIPT=$3
+    PORT=$4
+    
+    docker run --rm -it \
+    --user $UID:$(id -g) \
+    --net ${DOCKER_NETWORK} \
+    --workdir /${PROJECT_NAME} \
+    --env-file ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env \
+    -p ${PORT}:${PORT} \
+    -e KUBECONFIG=/.kube/config \
+    -v ${ENVIROMENTS_PATH}/${CUR_ENV}/${KUBE_CONFIG_DIR}/config:/.kube/config \
+    -v ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}:/${PROJECT_NAME} \
+    ${DOCKER_IMAGE} \
+    bash -c "${SCRIPT}"
+}
+
 # 進入 deploy-env 容器中的 Bash 環境，並且把 Volumes/{PROJECT_NAME} 的資料夾掛載進去 (使用 TTY 模式執行命令)
 exec_script_in_container_with_project() {
     PROJECT_NAME=$1
