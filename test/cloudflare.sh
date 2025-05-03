b206db2911757deaa87d3b06a4d804c3862a82ff
Maxime <machanwei@gmail.com>
2025-05-03 13:18:59 +0800

    test: cloudflare

--- test/cloudflare.sh
+++ test/cloudflare.sh
@@ -10,6 +10,6 @@ source ${KDE_SCRIPTS_PATH}/utils/cloudflare.sh
 cloudflare_login
 
 cloudflare_create_tunnel "test.maximema.dev" "http://localhost:8089"
+echo "Tunnel ID of test.maximema.dev: $(cloudflare_get_tunnel_id 'test.maximema.dev')"
 cloudflare_start_tunnel "test.maximema.dev" "host"
-
-# cloudflare_delete_tunnel "test.maximema.dev"
+cloudflare_delete_tunnel "test.maximema.dev"
