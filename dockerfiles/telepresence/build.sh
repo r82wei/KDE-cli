deec99759ca0328621bc3f3559802ac6c2ff677b
Maxime <machanwei@gmail.com>
2025-07-25 17:19:34 +0800

    fix: rebuild telepresence docker image

--- dockerfiles/telepresence/build.sh
+++ dockerfiles/telepresence/build.sh
@@ -3,5 +3,5 @@
 KUBECTL_VERSION=v1.32.0
 
 echo "Build telepresence image ..."
-# docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/telepresence:1.0.5-alpha-5 .
-docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/telepresence:1.0.5 .
+# docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/telepresence:1.0.6-alpha-1 .
+docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/telepresence:1.0.6 .
