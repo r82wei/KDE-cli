a9ddc76dc22a4fa1171e164a2f561bf2aff93ec5
Maxime <machanwei@gmail.com>
2025-08-23 15:42:15 +0800

    refact: code-server dockerfile build & release script

--- dockerfiles/code-server/build.sh
+++ dockerfiles/code-server/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-KDE_CLI_VERSION=v1.0.0-rc.1
-DOCKER_VERSION=v1.32.0
-DATE=$(date +%Y%m%d)
+source ./version.env
+DATE=$(date +%Y%m%d%H%M%S)
 
-echo "Build kde-code-server image ..."
-docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-code-server:${KDE_CLI_VERSION}-${DATE} .
+echo "Build kde-cli image ..."
+docker build --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-code-server:${KDE_CLI_VERSION}-${DATE} .
