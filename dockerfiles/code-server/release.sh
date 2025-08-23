31a049428a7dc3b916bf0816853b7779921fdfff
Maxime <machanwei@gmail.com>
2025-08-23 16:39:47 +0800

    feat: 更新 kde-code-server 和 kde-cli 的釋出腳本，新增無快取選項並標記最新版本

--- dockerfiles/code-server/release.sh
+++ dockerfiles/code-server/release.sh
@@ -3,4 +3,5 @@
 source ./version.env
 
 echo "Release kde-code-server ${KDE_CLI_VERSION} image ..."
-docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-code-server:${KDE_CLI_VERSION} .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-code-server:${KDE_CLI_VERSION} .
+docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-code-server:latest .
