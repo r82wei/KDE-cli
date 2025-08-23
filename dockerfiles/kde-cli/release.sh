a5441c745cf048411fde11be6c237031859e38e0
Maxime <machanwei@gmail.com>
2025-08-23 15:42:51 +0800

    feat: add kde-cli dockerfile & release script

---
+++ dockerfiles/kde-cli/release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./version.env
+
+echo "Release kde-cli ${KDE_CLI_VERSION} image ..."
+docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KDE_CLI_VERSION=${KDE_CLI_VERSION} -f Dockerfile -t r82wei/kde-cli:${KDE_CLI_VERSION} .
---
+++ dockerfiles/kde-cli/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KDE_CLI_VERSION=v1.0.0-rc.2
+DATE="20250823105008"
+
+read -e -p "Enter the project path: " PROJECT_PATH
+
+docker run -it --rm \
+    -w ${PROJECT_PATH} \
+    -v ${PROJECT_PATH}:${PROJECT_PATH} \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -e PUID=$UID \
+    -e PGID=$GID \
+    -e USER_NAME=$USER \
+    r82wei/kde-cli:${KDE_CLI_VERSION}-${DATE} \
+    bash
