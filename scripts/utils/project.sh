ea0a888e3674eddf9f9405dbc88b6e99c045f2d2
Maxime <machanwei@gmail.com>
2025-08-26 11:36:53 +0800

    feat: 加入 Anyong project 常用環境變數

--- scripts/utils/project.sh
+++ scripts/utils/project.sh
@@ -96,6 +96,14 @@ create_project() {
     read -p "請輸入專案部署環境 Image (執行 deploy 相關 shell 的環境，預設為 ${KDE_DEPLOY_ENV_IMAGE}): " DEPLOY_IMAGE
     DEPLOY_IMAGE=${DEPLOY_IMAGE:-${KDE_DEPLOY_ENV_IMAGE}}
     echo "DEPLOY_IMAGE=${DEPLOY_IMAGE}" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    # 匯出常用變數
+    echo "NPM_CONFIG_USERCONFIG=${PROJECT_PATH}/.npmrc" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "NPM_CONFIG_CACHE=${PROJECT_PATH}/.npm" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "CYPRESS_CACHE_FOLDER=${PROJECT_PATH}/.npm/.cache/Cypress" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "HELM_CONFIG_HOME=${PROJECT_PATH}/.helm/config" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "HELM_CACHE_HOME=${PROJECT_PATH}/.helm/cache" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "HELM_DATA_HOME=${PROJECT_PATH}/.helm/data" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
+    echo "HELM_PLUGINS=${PROJECT_PATH}/.helm/plugins" >> ${ENVIROMENTS_PATH}/${CUR_ENV}/${VOLUMES_DIR}/${PROJECT_NAME}/project.env
     init_project_deploy_script ${PROJECT_NAME}
     echo "專案 ${PROJECT_NAME} 已建立"
 }
