1dfc976902ecf7042042ac8d92e5f0ca071d9f85
Maxime <machanwei@gmail.com>
2026-01-29 19:21:17 +0800

    refactor: update build and release scripts for k9s image
    - Removed existing k9s directory before cloning the repository to ensure a clean build environment.
    - Updated build.sh and release.sh to clone the k9s repository directly from GitHub using the specified version.
    - Adjusted Docker build commands to streamline the image building process and ensure proper tagging.

--- dockerfiles/k9s/build.sh
+++ dockerfiles/k9s/build.sh
@@ -3,5 +3,10 @@
 K9S_VERSION=v0.50.18
 DATE=$(date +%Y%m%d%H%M%S)
 
-echo "Build k9s image ..."
-docker build --build-arg K9S_VERSION=${K9S_VERSION} -f Dockerfile -t r82wei/k9s:${K9S_VERSION}-${DATE} .
+if [ -d "k9s" ]; then
+    rm -rf k9s
+fi
+
+git clone -b ${K9S_VERSION} https://github.com/derailed/k9s.git
+cd k9s
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push -t r82wei/k9s:${K9S_VERSION}-${DATE} .
--- dockerfiles/k9s/release.sh
+++ dockerfiles/k9s/release.sh
@@ -2,6 +2,13 @@
 
 K9S_VERSION=v0.50.18
 
+if [ -d "k9s" ]; then
+    rm -rf k9s
+fi
+
+git clone -b ${K9S_VERSION} https://github.com/derailed/k9s.git
+cd k9s
+
 echo "Release k9s ${K9S_VERSION} image ..."
-docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push --build-arg K9S_VERSION=${K9S_VERSION} -f Dockerfile -t r82wei/k9s:${K9S_VERSION} .
-docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg K9S_VERSION=${K9S_VERSION} -f Dockerfile -t r82wei/k9s:latest .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push  -t r82wei/k9s:${K9S_VERSION} .
+docker buildx build --platform linux/amd64,linux/arm64 --push  -t r82wei/k9s:latest .
