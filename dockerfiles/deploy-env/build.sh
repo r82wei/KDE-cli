06a9be4380b50f1661c26ed6418689e6d09a8870
Maxime <machanwei@gmail.com>
2026-01-29 13:39:49 +0000

    feat: add gettext to deploy-env Dockerfile dependencies

--- dockerfiles/deploy-env/build.sh
+++ dockerfiles/deploy-env/build.sh
@@ -3,4 +3,4 @@
 KUBECTL_VERSION=v1.32.0
 
 echo "Build deploy-env image ..."
-docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/deploy-env:1.0.0 .
+docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg KUBECTL_VERSION=${KUBECTL_VERSION} -f Dockerfile -t r82wei/deploy-env:1.1.0 .
