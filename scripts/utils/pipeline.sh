86dee6378da558c075ebddea5532a7cf46a05793
Maxime <machanwei@gmail.com>
2026-02-05 18:11:49 +0000

    feat: add functionality to remove .pipeline.env file if it exists
    - Implemented a check in the execute_pipeline function to delete the .pipeline.env file if it is present in the project directory.
    - This enhancement ensures a clean environment for pipeline execution, preventing potential conflicts with existing environment variables.

--- scripts/utils/pipeline.sh
+++ scripts/utils/pipeline.sh
@@ -270,6 +270,11 @@ execute_pipeline() {
     
     # 載入專案環境變數
     source ${PROJECT_PATH}/project.env
+
+    # 如果 .pipeline.env 存在，則刪除
+    if [[ -f "${PROJECT_PATH}/.pipeline.env" ]]; then
+        rm ${PROJECT_PATH}/.pipeline.env
+    fi
     
     # 檢查是否使用自定義 Pipeline
     if [[ $(is_standard_cicd_pipeline "${KDE_PIPELINE_STAGES}") == "true" ]]; then
