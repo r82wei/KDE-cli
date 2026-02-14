75cfc9f27ce355b273da0cf8c923e122553b8a2d
Maxime <machanwei@gmail.com>
2026-02-14 10:19:39 +0000

    fix: improve user prompt handling in install script
    - Updated the user prompt in install.sh to ensure a newline is printed after the input, enhancing readability and user experience during installation.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ source ./local-install.sh
 
 cd ..
 # 詢問使用者是否要刪除安裝檔案
-read -p "是否要刪除安裝檔案？(y/n): " -n 1 -r
+read -p "是否要刪除安裝檔案？(y/n): " -n 1 -r && echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     rm -rf KDE-cli
