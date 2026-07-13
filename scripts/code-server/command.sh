501fb59737f142617d79bc8ac1f6b253417e3d26
Maxime <machanwei@anyong.com.tw>
2026-07-13 11:51:05 +0800

    feat(code-server): -v 可重複指定多個掛載目標

--- scripts/code-server/command.sh
+++ scripts/code-server/command.sh
@@ -10,7 +10,7 @@ show_help() {
     echo "  -d, --daemon        在背景執行"
     echo "  -p, --port          指定 code-server 的 port (預設為 8080)"
     echo "  -n, --name          指定 code-server 的容器名稱 (預設為 code-server，可用來同時啟動多個實例)"
-    echo "  -v, --volume        指定掛載到 container 的目錄 (預設為當前路徑)"
+    echo "  -v, --volume        指定掛載到 container 的目錄或檔案 (可重複指定多次，預設為當前路徑)"
     echo "  -w, --workdir       指定 code-server 開啟的資料夾 (預設與 --volume 相同，必須位於掛載目錄底下)"
     echo "  -h, --help          顯示此幫助訊息"
 }
@@ -18,7 +18,7 @@ show_help() {
 DAEMON=false
 PORT=8080
 NAME=code-server
-MOUNT_PATH=""
+MOUNT_PATHS=()
 OPEN_PATH=""
 
 # 參數解析（支援任意順序的 -d 與 -p <port>）
@@ -45,11 +45,11 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --volume|-v)
-            MOUNT_PATH="$2"
-            if [[ -z "${MOUNT_PATH}" ]]; then
-                echo "無效的掛載路徑：${MOUNT_PATH}"
+            if [[ -z "$2" ]]; then
+                echo "無效的掛載路徑"
                 exit 1
             fi
+            MOUNT_PATHS+=("$2")
             shift 2
             ;;
         --workdir|-w)
@@ -73,4 +73,4 @@ while [[ $# -gt 0 ]]; do
 done
 
 read -p "請輸入 code-server 的 password: " PASSWORD
-start_code_server ${PORT} ${DAEMON} ${NAME} "${MOUNT_PATH}" "${OPEN_PATH}"
+start_code_server "${PORT}" "${DAEMON}" "${NAME}" "${OPEN_PATH}" "${MOUNT_PATHS[@]}"
